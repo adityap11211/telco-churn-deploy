418952425d7930198f890aabd7f3785393e3815e
adityap11211 <aditya_1121@outlook.com>
2025-07-09 09:54:04 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import pickle
+
+with open("model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+st.title("Telco Churn Prediction")
+
+gender = st.selectbox("Gender", ["Male", "Female"])
+senior = st.selectbox("Senior Citizen", [0, 1])
+tenure = st.slider("Tenure (months)", 0, 72)
+monthly_charges = st.number_input("Monthly Charges", 0.0, 200.0)
+
+input_data = pd.DataFrame({
+    "gender": [gender],
+    "SeniorCitizen": [senior],
+    "tenure": [tenure],
+    "MonthlyCharges": [monthly_charges]
+})
+
+if st.button("Predict"):
+    prediction = model.predict(input_data)
+    st.write("Prediction:", "Churn" if prediction[0] == 1 else "No Churn")
